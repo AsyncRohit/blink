3512c0fa1677f2e4ead053278af5f2454f722794
Rohit <heartstealarrohitthakur@gmail.com>
2026-01-09 13:04:08 +0530

    validation of cartSchema

--- models/cart.js
+++ models/cart.js
@@ -1,16 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
-const cartSchema=mongoose.Schema({
-user:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"user",
-},
-products:[{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"product",
-},
-],
-totalPrice:Number,
-});
+/* ---------------- Cart Schema ---------------- */
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true
+    }
+  ],
+  totalPrice: {
+    type: Number,
+    required: true,
+    min: 0
+  }
+}, { timestamps: true });
 
-module.exports=mongoose("cart",cartSchema);
+/* ---------------- Joi Validation ---------------- */
+function validateCart(data) {
+  const schema = Joi.object({
+    user: Joi.string().length(24).required(),
+    products:Joi.array().items(joi.string().required()).required(),
+    totalPrice: Joi.number().min(0).required()
+  });
+
+  return schema.validate(data);
+}
+
+
+/* ---------------- Export ---------------- */
+module.exports = {
+  cartModel: mongoose.model("cart",cartSchema),
+  validateCart
+};
